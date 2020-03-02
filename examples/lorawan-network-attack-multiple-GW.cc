d409c214c3e883dda4ec0ad0d27d2b4dffbfbc0d
ivan <ivamarti@insa-rennes.fr>
2020-03-02 13:07:07 +0100

    fiwxing main

--- examples/lorawan-network-attack-multiple-GW.cc
+++ examples/lorawan-network-attack-multiple-GW.cc
@@ -250,7 +250,7 @@ PrintResults(uint32_t nGateways, uint32_t nDevices, uint32_t nJammers, double re
 			<< " " << cumulative_time_ed << " " << cumulative_time_jm
 			<< " " << cumulative_time_ed << " " << cumulative_time_jm
 			<< " " << edsentmsg << " " << nsmessagerx << " " <<  msgreceiveProb << " " << edretransmission
-			<< " " << edretransmission/nsmessagerx << " " << total_conso/nsmessagerx
+			<< " " << edretransmission/nsmessagerx << " " << total_conso
 			<< endl;
     //cumulative_time_ed << " " << cumulative_time_jm << endl;
 	Plot.close ();
